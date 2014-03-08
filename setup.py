d6ee6a5be8d78332892f72521f37f562a1c591a0
encorehu <huyoo353@126.com>
2014-03-08 21:39:50 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name = "webclient",
+        version="0.1.0",
+        packages = find_packages(),
+        zip_safe = False,
+
+        description = "a simple webbrowser simulate.",
+        long_description = "a simple webbrowser simulate.",
+        author = "encorehu",
+        author_email = "huyoo353@126.com",
+
+        license = "GPL",
+        keywords = ("webclient", "egg"),
+        platforms = "all",
+        url = "http://ddtcms.com",
+        )
